3adf5ddc1c6b800dd2617b5af5584bff4125a5b1
akonihakeem <akozymayor@gmail.com>
2022-10-06 02:36:59 +0100

    started the implementation of simple driver-passenger loaction sharing

---
+++ index.js
@@ -0,0 +1,33 @@
+// Setup basic express server
+const express = require('express');
+const app = express();
+const path = require('path');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log('Server listening at port %d', port);
+});
+
+// Routing
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Chatroom
+
+let numUsers = 0;
+const drivers = [];
+
+const passengers = [];
+
+io.on('connection', (socket) => {
+    const driversSpace = socket.of('/driver')
+    const passengersSpace = socket.of('/passengers')
+    driversSpace.on('put-driver-online', ({driverName, location}) => {
+        if (driverName && location) {
+            drivers.push({id: drivers.length + 1, driverName, location });
+
+            passengersSpace.emit('drivers-online', {drivers})
+        }
+    })
+});
